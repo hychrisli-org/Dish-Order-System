1fa3bf0d77d7cba7c81b385b7aae3393333c3b87
ww82 <ww82@duke.edu>
2017-12-10 13:11:28 -0800

    final modifications

--- src/main/java/cmpe/dos/service/RewardService.java
+++ src/main/java/cmpe/dos/service/RewardService.java
@@ -1,5 +1,6 @@
 package cmpe.dos.service;
 
+import cmpe.dos.dto.CouponDto;
 import cmpe.dos.entity.Reward;
 
 public interface RewardService {
@@ -7,4 +8,7 @@ public interface RewardService {
 	public Reward getValidCoupon(String couponId);
 	
 	public void DeleteUsedCoupon(Reward reward);
+
+	public Boolean addNewCoupon(CouponDto cDto);
+
 }
--- src/main/java/cmpe/dos/service/impl/RewardServiceImpl.java
+++ src/main/java/cmpe/dos/service/impl/RewardServiceImpl.java
@@ -1,28 +1,81 @@
 package cmpe.dos.service.impl;
 
+import cmpe.dos.dao.HibernateDao;
+import cmpe.dos.dao.RewardDao;
+import cmpe.dos.dto.CouponDto;
+import cmpe.dos.entity.CouponDict;
+import cmpe.dos.entity.Reward;
+import cmpe.dos.entity.User;
+import cmpe.dos.service.RewardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import cmpe.dos.dao.HibernateDao;
-import cmpe.dos.dao.RewardDao;
-import cmpe.dos.entity.Reward;
-import cmpe.dos.service.RewardService;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class RewardServiceImpl implements RewardService {
 
-	 @Autowired
-	 RewardDao dao;
-	 
+	@Autowired
+	RewardDao rewardDao;
+	@Autowired
+	HibernateDao<CouponDict> couponDictDao;
+
+	@Autowired
+	HibernateDao<User> userDao;
+
+
 	@Override
 	public Reward getValidCoupon(String couponId) {
 		// TODO Auto-generated method stub
-		return dao.getValidCoupon(couponId);
+		return rewardDao.getValidCoupon(couponId);
 	}
 
 	@Override
 	public void DeleteUsedCoupon(Reward reward) {
-		dao.deleteById(reward.getRewardId());
+		rewardDao.deleteById(reward.getRewardId());
 	}
 
+
+	@Override
+	public Boolean addNewCoupon(CouponDto couponDto) {
+
+		CouponDict coupon = couponDictDao.getById(couponDto.getCouponName());
+
+		if (coupon == null) {
+			CouponDict couponDict = new CouponDict();
+			couponDict.setCouponId(couponDto.getCouponName());
+			couponDict.setValue(couponDto.getCouponValue());
+			couponDictDao.create(couponDict);
+		}
+
+		// Expiration date of the coupon
+		Date today = Calendar.getInstance().getTime();
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		cal.add(Calendar.DATE, couponDto.getDuration());
+		Date endDate = cal.getTime();
+
+		Calendar calYears = Calendar.getInstance();
+		calYears.setTime(new Date());
+		calYears.add(Calendar.YEAR, -couponDto.getMemberYears());
+
+		Date ago = calYears.getTime();
+		List users = userDao.doQueryList(
+				"select b.username from User as a, Order as b where a.username = b.username "
+						+ "and a.signupDate <= ? group by b.username having count(*) > ?",
+				true, ago, Long.valueOf(couponDto.getOrderTimes().longValue()));
+
+		for (int i = 0; i < users.size(); i++) {
+			Reward reward = new Reward();
+			reward.setCouponId(couponDto.getCouponName());
+			reward.setUsername((String) (users.get(i)));
+			reward.setValidStart(today);
+			reward.setValidEnd(endDate);
+			rewardDao.create(reward);
+		}
+
+		return true;
+	}
 }
